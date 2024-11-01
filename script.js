fcb1104ec41182959bc90394eb08110592c2a069
geraldhi101 <gifthoni1010@gmail.com>
2024-11-02 06:14:11 +0700

    Add files

---
+++ script.js
@@ -0,0 +1,39 @@
+const nav = document.querySelector('#main');
+const headone = document.querySelector('h1');
+const all = document.querySelectorAll('*');
+const dark = document.querySelector('#dark');
+const light = document.querySelector('#light');
+
+let topOfNav = nav.offsetTop;
+
+    function fixNav() {
+      if (window.scrollY >= topOfNav) {
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+      } else {
+        document.body.classList.remove('fixed-nav');
+        document.body.style.paddingTop = 0;
+      }
+    }
+    
+    function darked(){
+      for (let i = 0; i < all.length; i++) {
+        all[i].style.background = 'black';
+        all[i].style.color = 'white';
+      }
+      document.body.style.backgroundColor = ' #eeeeee';
+      headone.style.textShadow = '3px 4px 0 rgba(255, 255, 255, 0.5)';
+    }
+    
+    function lighted(){
+      for (let i = 0; i < all.length; i++) {
+        all[i].style.background = 'white';
+        all[i].style.color = 'black';
+      }
+      document.body.style.backgroundColor = ' #eeeeee';
+      headone.style.textShadow = '3px 4px 0 rgba(0, 0, 0, 0.2)';
+    }
+    
+    window.addEventListener('scroll', fixNav);
+    dark.addEventListener('click', darked);
+    light.addEventListener('click', lighted);
